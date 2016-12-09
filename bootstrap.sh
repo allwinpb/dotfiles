c9cefb5c6ee073b56f3ca61792671e6b65e137c5
Allwin Baby Panikulam <allwinpb@gmail.com>
2016-12-09 14:37:18 +0800

    Added fonts as a submodule and some bookkeeping

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,20 +9,31 @@ function doIt() {
 	if [ -d "~/.oh-my-zsh" ]; then
 	  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	fi
+	# Install the powerline fonts
+	git submodule init
+	git submodule update
+	(cd fonts && ./install.sh)
 	# Install personal overrides to oh-my-zsh
-	rsync -avh --no-perms ./ohmyzsh_custom ~/.oh-my-zsh/custom
+	rsync -avh --no-perms ./ohmyzsh_custom/ ~/.oh-my-zsh/
 
 	# Install all the other stuff
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude ".macos" \
+		--exclude "fonts" \
+		--exclude "ohmyzsh_custom" \
 		--exclude "brew.sh" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	# Read the new configurations
+	if [ "$$" == "-zsh" -o "$$" == "zsh" ]; then
+		source ~/.zshrc;
+	elif [ "$$" == "-bash" -o "$$" == "bash" ]; then
+		source ~/.bash_profile;
+	fi;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
